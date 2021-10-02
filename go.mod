ae7701e07bc2a94c869c821908cad03b8e5561fc
Kanybek Momukeev <kmomukeev@win.tunduk.kg>
2021-10-02 12:21:34 +0600

    Working auth basic example

--- go.mod
+++ go.mod
@@ -2,4 +2,12 @@ module github.com/KanybekMomukeyev/echoauth
 
 go 1.16
 
-require github.com/labstack/echo/v4 v4.6.1 // indirect
+require (
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/labstack/echo/v4 v4.6.1 // indirect
+	github.com/mattn/go-colorable v0.1.11 // indirect
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
+	golang.org/x/net v0.0.0-20210929193557-e81a3d93ecf6 // indirect
+	golang.org/x/sys v0.0.0-20211001092434-39dca1131b70 // indirect
+	golang.org/x/time v0.0.0-20210723032227-1f47c861a9ac // indirect
+)
--- main.go
+++ main.go
@@ -1,16 +1,118 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
+	"time"
 
+	"github.com/golang-jwt/jwt"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
+
+const secretSigningKey = "secret132123123"
+
+type jwtCustomClaims struct {
+	Name  string `json:"name"`
+	UUID  string `json:"uuid"`
+	Admin bool   `json:"admin"`
+	jwt.StandardClaims
+}
+
+func login(c echo.Context) error {
+
+	m := make(map[string]string)
+	if err := c.Bind(&m); err != nil {
+		return err
+	}
+
+	jsonValue, _ := json.Marshal(m)
+	fmt.Printf("\n\n LOGIN REQ BODY JSON VALUE: %v\n", string(jsonValue))
+	fmt.Printf("\n\n LOGIN REQ BODY MAP VALUE %v\n", m)
+
+	if len(m["username"]) < 1 {
+		return echo.NewHTTPError(http.StatusUpgradeRequired, "username not specified")
+	}
+	if len(m["password"]) < 1 {
+		return echo.NewHTTPError(http.StatusUpgradeRequired, "password not specified")
+	}
+
+	username := m["username"]
+	password := m["password"]
+
+	if username != "pieter" || password != "claerhout" {
+		return echo.NewHTTPError(http.StatusForbidden, "user not found")
+	}
+
+	claims := &jwtCustomClaims{
+		Name:  "Pieter Claerhout",
+		UUID:  "9E98C454-C7AC-4330-B2EF-983765E00547",
+		Admin: true,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(time.Hour * 24 * 30).Unix(),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	t, err := token.SignedString([]byte(secretSigningKey))
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(http.StatusOK, map[string]string{
+		"token": t,
+	})
+}
+
+func accessible(c echo.Context) error {
+	return c.String(http.StatusOK, "Accessible")
+}
+
+func restricted(c echo.Context) error {
+	// --------- UNUSED STARTS --------- //
+	m := make(map[string]string)
+	if err := c.Bind(&m); err != nil {
+		return err
+	}
+	jsonValue, _ := json.Marshal(m)
+	fmt.Printf("\n\n RESTRICTED REQ BODY JSON VALUE: %v\n", string(jsonValue))
+	fmt.Printf("\n\n RESTRICTED REQ BODY MAP VALUE: %v\n", m)
+	// -------------------------------- //
+
+	user := c.Get("user").(*jwt.Token)
+	fmt.Printf("\n\n USER: %v\n\n", user)
+
+	claims := user.Claims.(*jwtCustomClaims)
+	fmt.Printf("\n\n CLAIMS: %v\n\n", claims)
+
+	name := claims.Name
+	uddid := claims.UUID
+	fmt.Printf("\n\n %v %v\n\n", name, uddid)
+
+	return c.JSON(http.StatusOK, claims)
+}
 
 func main() {
 	//http://localhost:1111/
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Use(middleware.CORS())
+
+	e.POST("/login", login)
+	e.GET("/", accessible)
+
+	r := e.Group("/restricted")
+	{
+		config := middleware.JWTConfig{
+			Claims:     &jwtCustomClaims{},
+			SigningKey: []byte(secretSigningKey),
+		}
+		r.Use(middleware.JWTWithConfig(config))
+		r.POST("", restricted)
+	}
+
 	e.Logger.Fatal(e.Start(":1111"))
 }
